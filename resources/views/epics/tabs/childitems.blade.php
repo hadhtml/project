2c68c1cd885b9e7df37441e09a9960602e900b8d
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2023-12-09 10:27:58 +0500

    new chanegs

--- resources/views/epics/tabs/childitems.blade.php
+++ resources/views/epics/tabs/childitems.blade.php
@@ -1,20 +1,20 @@
 <div class="row mb-3">
-    <div class="col-md-2">
+    <div class="col-md-1">
         50%
     </div>
-    <div class="col-md-10">
+    <div class="col-md-11 mt-2">
         <div class="progress">
-        <div class="progress-bar color-0AF30A" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width:20%">
-          <span class="sr-only">20% Complete</span>
-        </div>
-      </div>
+            <div class="progress-bar color-547AFF" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:80%">
+              <span class="sr-only">20% Complete</span>
+            </div>
+          </div>
     </div>
 </div>
 <div class="row">
     <div class="col-md-12 col-lg-12 col-xl-12 @if($epicstory->count() > 1) paddingrightzero @endif">
         <div class="d-flex flex-row align-items-center justify-content-between block-header">
             <div>
-                <h4><img src="{{ url('public/assets/svg/commentsmain.svg') }}"> Child Items</h4>
+                <h4><img src="{{ url('public/assets/svg/childitemssvg.svg') }}"> Child Items</h4>
             </div>
             <div class="displayflex">
                 <div class="dropdown firstdropdownofcomments">
@@ -38,54 +38,69 @@
                     <a class="dropdown-item" onclick="showorderby('asc',{{ $epic->id }},'flag_comments')" href="javascript:void(0)">Older</a>
                   </div>
                 </div>
-                <span onclick="writecomment()" class="btn btn-default btn-sm">Add</span>
+                <span data-toggle="collapse" data-target="#addstory" aria-expanded="false" aria-controls="addstory" class="btn btn-primary btn-sm" id="ButtonCollaps">Add</span>
             </div>
         </div>
     </div>
 </div>
-<div class="row mt-4 collapse" id="UpdateStory{{$epic->id}}">
+<div class="row mt-4 collapse" id="addstory">
     <div class="col-md-12 col-lg-12 col-xl-12">
-        <div class="card comment-card">
+        <div class="card comment-card storyaddcard">
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-12">
                         <div class="form-group mb-0">
-                            <input type="text" class="form-control" id="story_title{{$epic->id}}" required>
-                            <label for="objective-name">Title</label>
+                            <label for="objective-name">Item Title</label>
+                            <input type="text" class="form-control" required>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group mb-0">
+                            <label for="small-description">Assignee (optional)</label>
                             <select class="form-control" id="story_assign{{$epic->id}}">
                                 <option value="">Select Assignee</option>
-                                <?php foreach(DB::table('members')->where('org_user',Auth::id())->get() as $r){ ?>
+                                @foreach(DB::table('members')->where('org_user',Auth::id())->get() as $r)
                                   <option value="{{ $r->id }}">{{ $r->name }}</option>
-                                <?php }  ?>
+                                @endforeach
                             </select>
-                            <label for="small-description">Assignee</label>
                         </div>
                     </div>
               
                     <div class="col-md-6">
                         <div class="form-group mb-0">
-                           <select class="form-control" id="story_status{{$epic->id}}">
-                            <option value="">Select Status</option>
+                           <label for="small-description">Type</label>
+                           <select class="form-control">
+                            <option value="">Select Type</option>
                             <option value="To Do">To Do</option>
                             <option value="In progress">In Progress</option>
                              <option value="Done">Done</option>
                            </select>
-                            <label for="small-description">Status</label>
+                            
                         </div>
                     </div>
                 </div>
-
-                <div>
-                    <button type="button" onclick="addnewstory({{$epic->id}});" class="btn btn-primary btn-sm">Save</button>
-                </div>
-              
-                <div class="row">
-                    <div class="col-md-12">
-             
+                <div class="row mt-3">
+                    <div class="col-md-6">
+                        <div class="d-flex statusofstory">
+                            <h4>Status</h4>
+                            <div class="dropdown firstdropdownofcomments">
+                                  <span class="dropdown-toggle orderbybutton" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Done
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="7" viewBox="0 0 11 7" fill="none">
+                                      <path d="M10.8339 0.644857C10.6453 0.456252 10.3502 0.439106 10.1422 0.593419L10.0826 0.644857L5.49992 5.2273L0.917236 0.644857C0.72863 0.456252 0.433494 0.439106 0.225519 0.593419L0.165935 0.644857C-0.0226701 0.833463 -0.0398163 1.1286 0.114497 1.33657L0.165935 1.39616L5.12427 6.35449C5.31287 6.5431 5.60801 6.56024 5.81599 6.40593L5.87557 6.35449L10.8339 1.39616C11.0414 1.18869 11.0414 0.852323 10.8339 0.644857Z" fill="#787878"/>
+                                    </svg>
+                                  </span>
+                                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <a class="dropdown-item" href="javascript:void(0)">To Do</a>
+                                    <a class="dropdown-item" href="javascript:void(0)">In Progress</a>
+                                    <a class="dropdown-item" href="javascript:void(0)">Done</a>
+                                  </div>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 text-right">
+                        <span onclick="writecomment()" class="btn btn-default btn-sm">Cancel</span>
+                        <button type="submit" class="btn btn-primary btn-sm">Save</button>
                     </div>
                 </div>
             </div>
@@ -211,4 +226,9 @@
         </div>
     </div>
 </div>
+<script>
+    function addstory() {
+        $('#addstory').slideToggle();
+    }
+</script>
 @endforeach
