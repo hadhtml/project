01fa1c3366603457938e5b58492ca448006974df
basil-89 <84768962+basil-89@users.noreply.github.com>
2023-12-15 19:01:30 +0500

    Add files via upload

--- resources/views/Business-units/unit-backlog.blade.php
+++ resources/views/Business-units/unit-backlog.blade.php
@@ -231,6 +231,9 @@ $var_objective = "Backlog-Unit";
                                                             <button class="btn-circle btn-tolbar" data-toggle="modal" data-target="#delete{{$backlog->id}}">
                                                                 <img src="{{asset('public/assets/images/icons/delete.svg')}}" data-toggle="tooltip" data-placement="top" data-original-title="Delete">
                                                             </button>
+                                                            <a class="btn-circle btn-tolbar" href="{{url('epic-clone/'.$backlog->id.'/'.$organization->type)}}">
+                                                                <img src="{{asset('public/assets/images/icons/info-circle.svg')}}" data-toggle="tooltip" data-placement="top" data-original-title="Clone">
+                                                            </a>
                                                         </td>
                                                     </tr>
                                                     <div class="modal fade" id="delete{{$backlog->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
