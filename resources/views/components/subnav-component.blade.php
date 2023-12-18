3a9b320842c5c9db565d96ea7c68e488581dba49
basil-89 <84768962+basil-89@users.noreply.github.com>
2023-12-18 21:01:11 +0500

    Add files via upload

--- resources/views/components/subnav-component.blade.php
+++ resources/views/components/subnav-component.blade.php
@@ -1,4 +1,4 @@
-            @php
+            <!-- @php
             $organization = DB::table('organization')->where('user_id',Auth::id())->where('trash',NULL)->limit(2)->get();    
             @endphp  
             <div class="flex-shrink-0 p-3 bg-white sub-nav" id="panel" style="width: 280px; margin-top: 5%;">
@@ -23,17 +23,143 @@
                             </ul>
                         </div>
                     </li>
-                    <!-- <li class="mb-1">
-                        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
-                            Departments
+                </ul>
+            </div> -->
+
+
+            <div class="flex-shrink-0 p-3 bg-white sub-nav open" id="panel">
+                <button id="closeBtn" class="close-button">
+                    <img src="https://dev.agileprolific.com/public/assets/images/icons/collaps.svg">
+                </button>
+                <h6 class="title">Organization</h6>
+                <ul class="list-unstyled ps-0 expanded-navbar mb-0">
+                    <li class="mb-1">
+                        <button class="btn btn-toggle align-items-center rounded" data-toggle="collapse" data-target="#home-collapse" aria-expanded="true">
+                            <div class="d-flex flex-row align-items-center">
+                                <div class="mr-2">
+                                    <span style="font-size:22px" class="material-symbols-outlined">domain</span>
+                                </div>
+                                <div>
+                                    Business Units
+                                </div>
+                            </div>
                         </button>
-                        <div class="collapse" id="dashboard-collapse">
-                            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small px-3 py-2">
-                                <li class="py-2"><a href="#" class="link-dark rounded">Sales & Finance</a></li>
-                                <li class="py-2"><a href="#" class="link-dark rounded">Human Resources</a></li>
-                                <li class="py-2"><a href="#" class="link-dark rounded">IT Departments</a></li>
+
+                        @php
+                        $Stream = DB::table('business_units')->where('user_id',Auth::id())->get();
+                        @endphp  
+
+                        <div class="collapse" id="home-collapse" style="">
+                            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small px-1 py-2 nav-root-item">
+                                @if(count($Stream) > 0)
+                                @foreach($Stream as $value)
+                                    <li><a href="{{url('dashboard/organization/'.$value->slug.'/portfolio/'.$value->type)}}" class="link-dark rounded">{{$value->business_name}}</a></li>
+                                @endforeach
+                                @endif
                             </ul>
                         </div>
+                    </li>
+                </ul>
+
+                <ul class="list-unstyled ps-0 expanded-navbar-options">
+                    <li class="mb-1">
+                        <a href="{{url('dashboard/organization/Bu/dashboard')}}" class="d-flex flex-row align-items-center">
+                            <div class="mr-2">
+                                <span style="font-size:22px" class="material-symbols-outlined">auto_stories</span>
+                            </div>
+                            <div class="mr-2">
+                               Dashboard
+                            </div>
+                        </a>
+                    </li>
+                    <li class="mb-1">
+                        <a href="#" class="d-flex flex-row align-items-center">
+                            <div class="mr-2">
+                                <span style="font-size:22px" class="material-symbols-outlined">team_dashboard</span>
+                            </div>
+                            <div class="mr-2">
+                                Performance Dash.
+                            </div>
+                        </a>
+                    </li>
+                    <li class="mb-1">
+                        <a href="#" class="d-flex flex-row align-items-center">
+                            <div class="mr-2">
+                                 <span style="font-size:22px" class="material-symbols-outlined">map</span>
+                            </div>
+                            <div class="mr-2">
+                                OKR Mapper
+                            </div>
+                        </a>
+                    </li>
+                    <!-- Portfolio -->
+                    <li class="mb-1">
+                        <a href="#" class="d-flex flex-row align-items-center">
+                            <div class="mr-2">
+                                 <span style="font-size:22px" class="material-symbols-outlined">folder_supervised</span>
+                            </div>
+                            <div class="mr-2">
+                                OKR Planner
+                            </div>
+                        </a>
+                    </li>
+                    <li class="mb-1">
+                        <a href="#" class="d-flex flex-row align-items-center">
+                            <div class="mr-2">
+                                 <span style="font-size:22px" class="material-symbols-outlined">key_visualizer</span>
+                            </div>
+                            <div class="mr-2">
+                                Epic Backlog
+                            </div>
+                        </a>
+                    </li>
+                    <li class="mb-1">
+                        <a href="#" class="d-flex flex-row align-items-center">
+                            <div class="mr-2">
+                                <span style="font-size:22px" class="material-symbols-outlined">warning_off</span>
+                            </div>
+                            <div class="mr-2">
+                                Impediments
+                            </div>
+                            <!-- <div>
+                                <span class="badge btn-circle-xs badge-warning text-sm">2</span>
+                            </div> -->
+                        </a>
+                    </li>
+
+                    <li class="mb-1">
+                        <a href="#" class="d-flex flex-row align-items-center">
+                            <div class="mr-2">
+                                 <span style="font-size:22px" class="material-symbols-outlined">groups</span>
+                            </div>
+                            <div class="mr-2">
+                                Teams
+                            </div>
+                        </a>
+                    </li>
+
+                    <!-- <li class="mb-1">
+                        <a href="#" class="d-flex flex-row align-items-center">
+                            <div class="mr-2">
+                                 <span style="font-size:22px" class="material-symbols-outlined">sprint</span>
+                            </div>
+                            <div class="mr-2">
+                                Leadership Actions
+                            </div>
+                        </a>
+                    </li>
+                    <li class="mb-1">
+                        <a href="#" class="d-flex flex-row align-items-center">
+                            <div class="mr-2">
+                                <span style="font-size:22px" class="material-symbols-outlined">dangerous</span>
+                            </div>
+                            <div class="mr-2">
+                                Blockers
+                            </div>
+                            <div>
+                                <span class="badge btn-circle-xs badge-warning text-sm">10+</span>
+                            </div>
+                        </a>
                     </li> -->
                 </ul>
             </div>
