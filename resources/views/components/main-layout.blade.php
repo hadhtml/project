e758d8d2a328b43e44d25d999f6ea351799ac83b
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2023-12-18 11:33:53 +0500

    new chnages

--- resources/views/components/main-layout.blade.php
+++ resources/views/components/main-layout.blade.php
@@ -23,7 +23,11 @@
     
     <!-- Summernote -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.20/summernote-bs4.min.css" rel="stylesheet">
-    <script src="{{url('public/assets/Random-Pixel/dist/gixi-min.js')}}"></script> 
+
+    <!-- Icon family -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/4.0.0/font/MaterialIcons-Regular.ttf">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
@@ -198,6 +202,8 @@
             @endif
             
             
+            <!-- end Sidebar -->
+            <div class="content d-flex flex-column flex-column-fluid">
                 <!-- end breadcrums -->
                 <!-- begin page Content -->
                 <div class="container-fluid body-content">
@@ -354,6 +360,7 @@
                 @include('components.chart-breadcrumb')
                 @include('components.modal')    
                 @endif
+
                 <div class="body-inner-content">
                     @yield('content')
                 </div>
