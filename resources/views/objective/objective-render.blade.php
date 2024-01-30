3ed1b6d33abe00187f5dc4a5368a13f036dbe092
basil-89 <84768962+basil-89@users.noreply.github.com>
2024-01-30 12:10:11 +0500

    Add files via upload

--- resources/views/objective/objective-render.blade.php
+++ resources/views/objective/objective-render.blade.php
@@ -77,11 +77,11 @@ $keyweightcounte = DB::table('key_result')->wherenull('trash')->where('obj_id',$
          $trimmedStringobj = trim($objedit);
          @endphp
          <div class="action ml-0">
-            <button class="btn btn-icon btn-circle btn-tolbar ml-auto " onclick="editobjective({{$obj->id}},'{{$obj->objective_name}}','{{$obj->start_date}}','{{$obj->end_date}}','{{$trimmedStringobj}}','{{$obj->status}}')" data-toggle="modal" data-target="#edit-objective">
+            <button class="btn btn-icon btn-circle btn-tolbar ml-auto " onclick="editobjective(event,{{$obj->id}},'{{$obj->objective_name}}','{{$obj->start_date}}','{{$obj->end_date}}','{{$trimmedStringobj}}','{{$obj->status}}')" data-toggle="modal" data-target="#edit-objective">
             <img src="{{ asset('public/assets/images/icons/edit.svg') }}" alt="Edit"
                style="border-radius: 50%; width: 18px; height: 18px;">
             </button>
-            <button class="btn btn-icon btn-circle btn-tolbar delete-obj mr-2" onclick="deleteobj({{$obj->id}})" data-toggle="modal" data-target="#delete-objective">
+            <button class="btn btn-icon btn-circle btn-tolbar delete-obj mr-2" onclick="deleteobj(event,{{$obj->id}})" data-toggle="modal" data-target="#delete-objective">
             <img src="{{ asset('public/assets/images/icons/delete.svg') }}" alt="Delete"
                style="border-radius: 50%; width: 18px; height: 18px;">
             </button>
@@ -274,13 +274,13 @@ $keyweightcounte = DB::table('key_result')->wherenull('trash')->where('obj_id',$
                                              @endphp
                                              <div class="action ml-0">
                                                 <button
-                                                   class="btn btn-icon btn-circle bg-white btn-tolbar ml-auto" onclick="editinitiative({{$initiative->id}},'{{$initiative->initiative_name}}','{{$initiative->initiative_start_date}}','{{$initiative->initiative_end_date}}','{{$trimmedStringinit}}','{{$initiative->initiative_weight}}','{{$key->id}}','{{$obj->id}}','{{$key->key_end_date}}')" data-toggle="modal" data-target="#edit-initiative">
+                                                   class="btn btn-icon btn-circle bg-white btn-tolbar ml-auto" onclick="editinitiative(event,{{$initiative->id}},'{{$initiative->initiative_name}}','{{$initiative->initiative_start_date}}','{{$initiative->initiative_end_date}}','{{$trimmedStringinit}}','{{$initiative->initiative_weight}}','{{$key->id}}','{{$obj->id}}','{{$key->key_end_date}}')" data-toggle="modal" data-target="#edit-initiative">
                                                 <img src="{{ asset('public/assets/images/icons/edit.svg') }}"
                                                    alt="Edit"
                                                    style="border-radius: 50%; width: 18px; height: 18px;">
                                                 </button>
                                                 <button
-                                                   class="btn btn-icon btn-circle bg-white btn-tolbar" onclick="deletekeyinitiative({{$initiative->id}},'{{$key->id}}','{{$obj->id}}')" data-toggle="modal" data-target="#delete-initiative-key">
+                                                   class="btn btn-icon btn-circle bg-white btn-tolbar" onclick="deletekeyinitiative(event,{{$initiative->id}},'{{$key->id}}','{{$obj->id}}')" data-toggle="modal" data-target="#delete-initiative-key">
                                                 <img src="{{ asset('public/assets/images/icons/delete.svg') }}"
                                                    alt="Delete"
                                                    style="border-radius: 50%; width: 18px; height: 18px;">
