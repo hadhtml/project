754115de2cd76d0825045b0e02097655d21415f9
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2024-02-06 16:26:06 +0500

    asdsad

--- database/migrations/2024_02_06_112055_create_flow_chart_scripts_table.php
+++ database/migrations/2024_02_06_112055_create_flow_chart_scripts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEscalateCardsTable extends Migration
+class CreateFlowChartScriptsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateEscalateCardsTable extends Migration
      */
     public function up()
     {
-        Schema::create('escalate_cards', function (Blueprint $table) {
+        Schema::create('flow_chart_scripts', function (Blueprint $table) {
             $table->id();
-            $table->string('flag_id');
-            $table->string('from');
-            $table->string('to');
+            $table->string('unit_id')->nullable();
+            $table->string('type')->nullable();
+            $table->longtext('script')->nullable();
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateEscalateCardsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('escalate_cards');
+        Schema::dropIfExists('flow_chart_scripts');
     }
 }
--- resources/views/mapper/unit/scripts.blade.php
+++ resources/views/mapper/unit/scripts.blade.php
@@ -10,16 +10,7 @@ const dataToImport = {
                     "html": '<div class="row"><div class="col-md-4"> <div class="buisnessunit"> <div class="mainheading row mb-3"> <div class="col-md-12"> <h4>{{$data->business_name}}</h4> </div> </div> @foreach(DB::table('objectives')->where('type' , 'unit')->where('unit_id'  ,$data->id)->get() as $o) <div class="row"> <div class="col-md-1"> <img src="{{ url('public/assets/svg/linkingbuisnessunit.svg') }}"> </div> <div class="col-md-8"> <div class="buisnessunit-card-subtittle"> <p class="buisnessunitheading">{{ $o->objective_name }}</p> </div> </div> <div class="col-md-3 text-right"> <div class="badge bg-success buisnessunitbadge"> {{ $o->obj_prog }}% </div> </div> <div class="col-md-12"> @foreach(DB::table('key_result')->where('obj_id' , $o->id)->get() as $key_result) <div class="row mt-2"> <div class="col-md-1"> <img src="{{ url('public/assets/svg/linkingkey.svg') }}"> </div> <div class="col-md-7"> <p class="buisnessunitlinkingtext">{{$key_result->key_name}}</p> </div> <div class="col-md-1"> @if(DB::table('team_link_child')->where('bussiness_key_id' , $key_result->id)->count() > 0) <img src="{{ url('public/assets/svg/link.svg') }}"> @endif </div> <div class="col-md-3 text-right"> <div class="badge buisnessunitbadge">{{$key_result->key_prog}}%</div> </div> </div> @endforeach </div> </div> @endforeach </div> </div></div>',
                     "typenode": false,
                     "inputs": {},
-                    "outputs": {
-                        @foreach(DB::table('team_link_child')->where('from' , 'unit')->where('bussiness_unit_id'  ,$data->id)->get() as $key=> $t)
-                        "output_{{$key+1}}": {
-                            "connections": [{
-                                "node": "10{{$key+1}}",
-                                "output": "input_1"
-                            }]
-                        },
-                        @endforeach
-                    },
+                    {!! DB::table('team_link_child')->first()->test !!}
                     "pos_x": 10,
                     "pos_y": 20
                 },
