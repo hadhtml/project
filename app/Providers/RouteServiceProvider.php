885e5921632132628502354bd060c6d96428affc
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2024-02-14 15:20:18 +0500

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard/organizations';
+    public const HOME = '/organization/dashboard';
 
     /**
      * The controller namespace for the application.
