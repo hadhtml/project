0b6a83e1da8068b21ecd72c351f03422463df623
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2024-02-22 12:19:28 +0500

    asdasd

--- resources/views/mapper/org/index.blade.php
+++ resources/views/mapper/org/index.blade.php
@@ -18,7 +18,15 @@ $var_objective = "mapper-org";
             <div class="slot slot-inactive">
                 <span class="material-symbols-outlined f-18">location_searching</span>
                 <div class="slot-label" ><span class="label-text">{{ $o->objective_name }}</span></div>
-                <div class="badge-inprogress">{{round($o->obj_prog,0)}}%</div>
+                @if($o->status == 'In progress')
+                <span class="badge-cs warning"style="width: 25px;">{{round($o->obj_prog,0)}}%</span>
+                @endif
+                @if($o->status == 'Done')
+                <span class="badge-cs success"style="width: 25px;">{{round($o->obj_prog,0)}}%</span>
+                @endif
+                @if($o->status == 'To Do')
+                <span class="badge-cs bg-secondary"style="width: 25px;">{{round($o->obj_prog,0)}}%</span>
+                @endif
             </div>
             @foreach(DB::table('key_result')->wherenull('trash')->where('obj_id' , $o->id)->get() as $k)
             <div class="slot-active">
