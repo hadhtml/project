35746f60e45435a3626992d844b623d7b599c292
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2024-03-06 13:47:39 +0500

    sdcsd

--- resources/views/mapper/unit/index.blade.php
+++ resources/views/mapper/unit/index.blade.php
@@ -18,13 +18,29 @@ $var_objective = "mapper-unit";
             <div class="slot slot-inactive">
                 <span class="material-symbols-outlined f-18">location_searching</span>
                 <div class="slot-label" ><span class="label-text">{{ $o->objective_name }}</span></div>
+                @if($o->status == 'Done')
+                <div class="badge-done">{{round($o->obj_prog,0)}}%</div>
+                @endif
+                @if($o->status == 'To Do')
+                <div class="badge-todo">{{round($o->obj_prog,0)}}%</div>
+                @endif
+                @if($o->status == 'In progress')
                 <div class="badge-inprogress">{{round($o->obj_prog,0)}}%</div>
+                @endif
             </div>
             @foreach(DB::table('key_result')->wherenull('trash')->where('obj_id' , $o->id)->get() as $k)
             <div class="slot-active">
                 <span class="material-symbols-outlined f-18 ml-2">key</span>
                 <div class="slot-label"><span class="label-text">{{ $k->key_name }}</span></div>
-                <div class="badge-todo mr-2">{{ $k->key_prog }}%</div>
+                @if($k->key_status == 'Done')
+               <div class="badge-done mr-2"">{{round($k->key_prog,0)}}%</div>
+               @endif
+               @if($k->key_status == 'To Do')
+               <div class="badge-todo mr-2"">{{round($k->key_prog,0)}}%</div>
+               @endif
+               @if($k->key_status == 'In progress')
+               <div class="badge-inprogress mr-2"">{{round($k->key_prog,0)}}%</div>
+               @endif
                 <div id="buisness_unit_key_result_{{ $k->id }}" class="slot-anchor-small @if(DB::table('team_link_child')->where('bussiness_key_id' , $k->id)->count() > 0) slot-anchor-active @else slot-anchor-inactive @endif"></div>
             </div>
             @endforeach
@@ -40,7 +56,15 @@ $var_objective = "mapper-unit";
                 <div id="connectedobjective{{ $o->id }}" class="slot-anchor-small slot-anchor-active drag-impo-grab"></div>
                 <span class="material-symbols-outlined f-18">location_searching</span>
                 <div class="slot-label" ><span class="label-text">{{ $o->objective_name }}</span></div>
+                @if($o->status == 'Done')
+                <div class="badge-done">{{round($o->obj_prog,0)}}%</div>
+                @endif
+                @if($o->status == 'To Do')
+                <div class="badge-todo">{{round($o->obj_prog,0)}}%</div>
+                @endif
+                @if($o->status == 'In progress')
                 <div class="badge-inprogress">{{round($o->obj_prog,0)}}%</div>
+                @endif
             </div>
             @endforeach
           </div>
@@ -57,13 +81,29 @@ $var_objective = "mapper-unit";
               <div id="connectedobjective{{ $o->id }}" class="slot-anchor-small slot-anchor-active drag-impo-grab"></div>
               <span class="material-symbols-outlined f-18">location_searching</span>
               <div class="slot-label drag-impo-grab"><span class="label-text">{{ $o->objective_name }}</span></div>
+              @if($o->status == 'Done')
+              <div class="badge-done">{{round($o->obj_prog,0)}}%</div>
+              @endif
+              @if($o->status == 'To Do')
+              <div class="badge-todo">{{round($o->obj_prog,0)}}%</div>
+              @endif
+              @if($o->status == 'In progress')
               <div class="badge-inprogress">{{round($o->obj_prog,0)}}%</div>
+              @endif
             </div>
             @foreach(DB::table('key_result')->wherenull('trash')->where('obj_id' , $o->id)->get() as $k)
             <div class="slot-active">
                 <span class="material-symbols-outlined f-18 ml-2">key</span>
                 <div class="slot-label"><span class="label-text">{{ $k->key_name }}</span></div>
-                <div class="badge-todo mr-2">{{ $k->key_prog }}%</div>
+                @if($k->key_status == 'Done')
+                 <div class="badge-done mr-2"">{{round($k->key_prog,0)}}%</div>
+                 @endif
+                 @if($k->key_status == 'To Do')
+                 <div class="badge-todo mr-2"">{{round($k->key_prog,0)}}%</div>
+                 @endif
+                 @if($k->key_status == 'In progress')
+                 <div class="badge-inprogress mr-2"">{{round($k->key_prog,0)}}%</div>
+                 @endif
                 <div id="buisness_unit_key_result_{{ $k->id }}" class="slot-anchor-small @if(DB::table('team_link_child')->where('bussiness_key_id' , $k->id)->count() > 0) slot-anchor-active @else slot-anchor-inactive @endif"></div>
             </div>
             @endforeach
@@ -79,7 +119,15 @@ $var_objective = "mapper-unit";
                   <div id="connectedobjective{{ $o->id }}" class="slot-anchor-small slot-anchor-active drag-impo-grab"></div>
                   <span class="material-symbols-outlined f-18">location_searching</span>
                   <div class="slot-label" ><span class="label-text">{{ $o->objective_name }}</span></div>
+                  @if($o->status == 'Done')
+                  <div class="badge-done">{{round($o->obj_prog,0)}}%</div>
+                  @endif
+                  @if($o->status == 'To Do')
+                  <div class="badge-todo">{{round($o->obj_prog,0)}}%</div>
+                  @endif
+                  @if($o->status == 'In progress')
                   <div class="badge-inprogress">{{round($o->obj_prog,0)}}%</div>
+                  @endif
               </div>
               @endforeach
             </div>
