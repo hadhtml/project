7afca98f6a74193b5d2431d863ac121a456e325f
basil-89 <84768962+basil-89@users.noreply.github.com>
2024-03-13 12:39:39 +0500

    Add files via upload

--- resources/views/mapper/org/index.blade.php
+++ resources/views/mapper/org/index.blade.php
@@ -82,25 +82,11 @@ $var_objective = "mapper-org";
    window.addEventListener("load", function() {
    "use strict";
    
-
-   @foreach(DB::table('team_link_child')->groupby('bussiness_key_id')->where('user_id' , Auth::id())->orWhere('user_id', Auth::user()->invitation_id)->get() as $t_l_c)
    @foreach(DB::table('team_link_child')->groupby('bussiness_key_id')->where('user_id' , Auth::id())->get() as $t_l_c)
-
    var slout_out_buisness_unit_key_result_{{ $t_l_c->bussiness_key_id }} = document.getElementById("buisness_unit_key_result_{{ $t_l_c->bussiness_key_id }}");
    @endforeach
    
    
-
-   @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->orWhere('user_id', Auth::user()->invitation_id)->get() as $in_t_l_c)
-   var connectedobjective{{ $in_t_l_c->linked_objective_id }} = document.getElementById("connectedobjective{{ $in_t_l_c->linked_objective_id }}");
-   @endforeach
-   
-   @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->orWhere('user_id', Auth::user()->invitation_id)->get() as $linedeclarekeyforslot =>  $line_t_l_c)
-   var line{{ $linedeclarekeyforslot+1 }};
-   @endforeach
-   
-   @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->orWhere('user_id', Auth::user()->invitation_id)->get() as $linekeyforslot =>  $line_t_l_c)
-
    @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->get() as $in_t_l_c)
    var connectedobjective{{ $in_t_l_c->linked_objective_id }} = document.getElementById("connectedobjective{{ $in_t_l_c->linked_objective_id }}");
    @endforeach
@@ -110,7 +96,6 @@ $var_objective = "mapper-org";
    @endforeach
    
    @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->get() as $linekeyforslot =>  $line_t_l_c)
-
    line{{$linekeyforslot+1}} = new LeaderLine(connectedobjective{{ $line_t_l_c->linked_objective_id }}, slout_out_buisness_unit_key_result_{{ $line_t_l_c->bussiness_key_id }}, {
    startPlug: "behind",
    endPlug: "behind",
@@ -129,21 +114,13 @@ $var_objective = "mapper-org";
    
    new PlainDraggable(node_1, {
    onMove: function() {
-
-   @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->orWhere('user_id', Auth::user()->invitation_id)->get() as $draglinekey =>  $drag)
-
    @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->get() as $draglinekey =>  $drag)
-
    line{{ $draglinekey+1 }}.position();
    @endforeach
    },
    // onMoveStart: function() { line.dash = {animation: true}; },
    onDragEnd: function() {
-
-      @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->orWhere('user_id', Auth::user()->invitation_id)->get() as $draglinekey =>  $drag)
-
       @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->get() as $draglinekey =>  $drag)
-
          line{{ $draglinekey+1 }}.dash = false;
          @endforeach
    },
@@ -155,21 +132,13 @@ $var_objective = "mapper-org";
 
    new PlainDraggable(orgteam{{ $o_t->id }}, {
    onMove: function() {
-
-   @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->orWhere('user_id', Auth::user()->invitation_id)->get() as $draglinekey =>  $drag)
-
    @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->get() as $draglinekey =>  $drag)
-
    line{{ $draglinekey+1 }}.position();
    @endforeach
    },
    // onMoveStart: function() { line.dash = {animation: true}; },
    onDragEnd: function() {
-
-   @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->orWhere('user_id', Auth::user()->invitation_id)->get() as $draglinekey =>  $drag)
-
    @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->get() as $draglinekey =>  $drag)
-
          line{{ $draglinekey+1 }}.dash = false;
          @endforeach
    },
@@ -184,21 +153,13 @@ $var_objective = "mapper-org";
    @foreach(DB::table('unit_team')->where('org_id'  , $b->id)->get() as $b_t)
    new PlainDraggable(buisnessunitteam{{ $b_t->id }}, {
    onMove: function() {
-
-   @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->orWhere('user_id', Auth::user()->invitation_id)->get() as $draglinekey =>  $drag)
-
    @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->get() as $draglinekey =>  $drag)
-
    line{{ $draglinekey+1 }}.position();
    @endforeach
    },
    // onMoveStart: function() { line.dash = {animation: true}; },
    onDragEnd: function() {
-
-   @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->orWhere('user_id', Auth::user()->invitation_id)->get() as $draglinekey =>  $drag)
-
    @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->get() as $draglinekey =>  $drag)
-
          line{{ $draglinekey+1 }}.dash = false;
          @endforeach
    },
@@ -207,21 +168,13 @@ $var_objective = "mapper-org";
    @endforeach
    new PlainDraggable(buisnessunit{{ $b->id }}, {
    onMove: function() {
-
-   @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->orWhere('user_id', Auth::user()->invitation_id)->get() as $draglinekey =>  $drag)
-
    @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->get() as $draglinekey =>  $drag)
-
    line{{ $draglinekey+1 }}.position();
    @endforeach
    },
    // onMoveStart: function() { line.dash = {animation: true}; },
    onDragEnd: function() {
-
-   @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->orWhere('user_id', Auth::user()->invitation_id)->get() as $draglinekey =>  $drag)
-
    @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->get() as $draglinekey =>  $drag)
-
          line{{ $draglinekey+1 }}.dash = false;
          @endforeach
    },
@@ -230,21 +183,13 @@ $var_objective = "mapper-org";
    @foreach($valuestream as $v)
    new PlainDraggable(valuestream{{ $v->id }}, {
    onMove: function() {
-
-   @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->orWhere('user_id', Auth::user()->invitation_id)->get() as $draglinekey =>  $drag)
-
    @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->get() as $draglinekey =>  $drag)
-
    line{{ $draglinekey+1 }}.position();
    @endforeach
    },
    // onMoveStart: function() { line.dash = {animation: true}; },
    onDragEnd: function() {
-
-   @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->orWhere('user_id', Auth::user()->invitation_id)->get() as $draglinekey =>  $drag)
-
    @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->get() as $draglinekey =>  $drag)
-
          line{{ $draglinekey+1 }}.dash = false;
          @endforeach
    },
@@ -255,21 +200,13 @@ $var_objective = "mapper-org";
    
     new PlainDraggable(valuestreamteam{{ $v_t->id }}, {
        onMove: function() {
-
-         @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->orWhere('user_id', Auth::user()->invitation_id)->get() as $draglinekey =>  $drag)
-
          @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->get() as $draglinekey =>  $drag)
-
          line{{ $draglinekey+1 }}.position();
          @endforeach
        },
        // onMoveStart: function() { line.dash = {animation: true}; },
        onDragEnd: function() {
-
-         @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->orWhere('user_id', Auth::user()->invitation_id)->get() as $draglinekey =>  $drag)
-
          @foreach(DB::table('team_link_child')->where('user_id' , Auth::id())->get() as $draglinekey =>  $drag)
-
          line{{ $draglinekey+1 }}.dash = false;
          @endforeach
        },
