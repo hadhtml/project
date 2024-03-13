670659da08c8ace5bbde2498b3be13041e297ed8
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2024-03-13 19:34:32 +0500

    asdas

--- resources/views/mapper/unit/index.blade.php
+++ resources/views/mapper/unit/index.blade.php
@@ -69,14 +69,14 @@ $var_objective = "mapper-unit";
           </div>
           @foreach(DB::table('unit_team')->where('org_id'  , $data->id)->get() as $b_t)
           <div id="buisnessunitteam{{ $b_t->id }}" class="node buisnessunitsteam">
-            <div class="node-name slot-active drag-impo-grab">
-              <div class="slot-label drag-impo-grab"><span class="mr-2 d-flex badge-team-valuestream">BU <span style="font-size:22px" class="material-symbols-outlined ml-2">groups</span></span>  {{ $b_t->team_title }}</div>
-            </div>
+            <a href="{{ url('dashboard/organization') }}/{{ $b_t->slug }}/dashboard/BU" target="_blank" class="blanklink node-name slot-active drag-impo-grab">
+                <div class="slot-label drag-impo-grab"><span class="mr-2 d-flex badge-team-valuestream">BU <span style="font-size:22px" class="material-symbols-outlined ml-2">groups</span></span>  {{ $b_t->team_title }}</div>
+             </a>
             @foreach(DB::table('objectives')->wherenull('trash')->where('unit_id' , $b_t->id)->where('type' , 'BU')->get() as $o)
-            <div class="slot slot-inactive">
-                <div id="connectedobjective{{ $o->id }}" class="slot-anchor-small slot-anchor-active drag-impo-grab"></div>
+            <a href="{{ url('dashboard/organization') }}/{{ $b_t->slug }}/portfolio/BU?objective={{ $o->id }}" target="_blank" class="blanklink @if(DB::table('team_link_child')->where('linked_objective_id' , $o->id)->count() > 0) slot-active @else slot-inactive @endif drag-impo-grab">
+                <div id="connectedobjective{{ $o->id }}" class="slot-anchor-small @if(DB::table('team_link_child')->where('linked_objective_id' , $o->id)->count() > 0) slot-anchor-active @else slot-anchor-inactive @endif drag-impo-grab"></div>
                 <span class="material-symbols-outlined f-18">location_searching</span>
-                <div class="slot-label" ><span class="label-text">{{ $o->objective_name }}</span></div>
+                <div class="slot-label drag-impo-grab"><span class="label-text">{{ $o->objective_name }}</span></div>
                 @if($o->status == 'Done')
                 <div class="badge-done">{{round($o->obj_prog,0)}}%</div>
                 @endif
@@ -86,70 +86,70 @@ $var_objective = "mapper-unit";
                 @if($o->status == 'In progress')
                 <div class="badge-inprogress">{{round($o->obj_prog,0)}}%</div>
                 @endif
-            </div>
+             </a>
             @endforeach
           </div>
           @endforeach
           <!-- Node 1 End -->
           <!-- Node 2 -->
-          @foreach(DB::table('value_stream')->where('unit_id'  , $data->id)->orderby('id' , 'asc')->limit(4)->get() as $key_calue_stream => $v)
+          @foreach(DB::table('value_stream')->where('unit_id'  , $data->id)->get() as $key_calue_stream => $v)
           <div id="valuestream{{ $v->id }}" class="node valuestreambox">
-            <div class="node-name slot-active drag-impo-grab">
-              <div class="slot-label drag-impo-grab"><span style="font-size:22px" class="material-symbols-outlined">layers</span> {{ $v->value_name }}</div>
-            </div>
+            <a href="{{ url('dashboard/organization') }}/{{ $v->slug }}/dashboard/stream" target="_blank" class="blanklink node-name slot-active drag-impo-grab">
+               <div class="slot-label drag-impo-grab"><span style="font-size:22px" class="material-symbols-outlined mr-2">layers</span> {{ $v->value_name }}</div>
+            </a>
             @foreach(DB::table('objectives')->wherenull('trash')->where('unit_id' , $v->id)->where('type' , 'stream')->get() as $o)
-            <div class="@if(DB::table('team_link_child')->where('linked_objective_id' , $o->id)->count() > 0) slot-active @else slot-inactive @endif drag-impo-grab">
-              <div id="connectedobjective{{ $o->id }}" class="slot-anchor-small slot-anchor-active drag-impo-grab"></div>
-              <span class="material-symbols-outlined f-18">location_searching</span>
-              <div class="slot-label drag-impo-grab"><span class="label-text">{{ $o->objective_name }}</span></div>
-              @if($o->status == 'Done')
-              <div class="badge-done">{{round($o->obj_prog,0)}}%</div>
-              @endif
-              @if($o->status == 'To Do')
-              <div class="badge-todo">{{round($o->obj_prog,0)}}%</div>
-              @endif
-              @if($o->status == 'In progress')
-              <div class="badge-inprogress">{{round($o->obj_prog,0)}}%</div>
-              @endif
-            </div>
+            <a href="{{ url('dashboard/organization') }}/{{ $v->slug }}/portfolio/stream?objective={{ $o->id }}" target="_blank" class="blanklink @if(DB::table('team_link_child')->where('linked_objective_id' , $o->id)->count() > 0) slot-active @else slot-inactive @endif drag-impo-grab">
+               <div id="connectedobjective{{ $o->id }}" class="slot-anchor-small @if(DB::table('team_link_child')->where('linked_objective_id' , $o->id)->count() > 0) slot-anchor-active @else slot-anchor-inactive @endif drag-impo-grab"></div>
+               <span class="material-symbols-outlined f-18">location_searching</span>
+               <div class="slot-label drag-impo-grab"><span class="label-text">{{ $o->objective_name }}</span></div>
+               @if($o->status == 'Done')
+               <div class="badge-done">{{round($o->obj_prog,0)}}%</div>
+               @endif
+               @if($o->status == 'To Do')
+               <div class="badge-todo">{{round($o->obj_prog,0)}}%</div>
+               @endif
+               @if($o->status == 'In progress')
+               <div class="badge-inprogress">{{round($o->obj_prog,0)}}%</div>
+               @endif
+            </a>
             @foreach(DB::table('key_result')->wherenull('trash')->where('obj_id' , $o->id)->get() as $k)
-            <div class="slot-active">
-                <span class="material-symbols-outlined f-18 ml-2">key</span>
-                <div class="slot-label"><span class="label-text">{{ $k->key_name }}</span></div>
-                @if($k->key_status == 'Done')
-                 <div class="badge-done mr-2"">{{round($k->key_prog,0)}}%</div>
-                 @endif
-                 @if($k->key_status == 'To Do')
-                 <div class="badge-todo mr-2"">{{round($k->key_prog,0)}}%</div>
-                 @endif
-                 @if($k->key_status == 'In progress')
-                 <div class="badge-inprogress mr-2"">{{round($k->key_prog,0)}}%</div>
-                 @endif
-                <div id="buisness_unit_key_result_{{ $k->id }}" class="slot-anchor-small @if(DB::table('team_link_child')->where('bussiness_key_id' , $k->id)->count() > 0) slot-anchor-active @else slot-anchor-inactive @endif"></div>
-            </div>
+            <a href="{{ url('dashboard/organization') }}/{{ $v->slug }}/portfolio/stream?keyresult={{ $k->id }}" target="_blank" class="blanklink slot-active">
+               <span class="material-symbols-outlined f-18 ml-2">key</span>
+               <div class="slot-label"><span class="label-text">{{ $k->key_name }}</span></div>
+               @if($k->key_status == 'Done')
+               <div class="badge-done mr-2">{{round($k->key_prog,0)}}%</div>
+               @endif
+               @if($k->key_status == 'To Do')
+               <div class="badge-todo mr-2">{{round($k->key_prog,0)}}%</div>
+               @endif
+               @if($k->key_status == 'In progress')
+               <div class="badge-inprogress mr-2">{{round($k->key_prog,0)}}%</div>
+               @endif
+               <div id="buisness_unit_key_result_{{ $k->id }}" class="slot-anchor-small @if(DB::table('team_link_child')->where('bussiness_key_id' , $k->id)->count() > 0) slot-anchor-active @else slot-anchor-inactive @endif"></div>
+            </a>
             @endforeach
             @endforeach
           </div>
             @foreach(DB::table('value_team')->where('org_id'  , $v->id)->get() as $key_value_stream_team => $v_t)
             <div id="valuestreamteam{{ $v_t->id }}" class="node valuestreamteambox">
-              <div class="node-name slot-active drag-impo-grab">
-                <div class="slot-label drag-impo-grab"><span class="mr-2 d-flex badge-team-valuestream">VS <span style="font-size:22px" class="material-symbols-outlined ml-2">groups</span></span>  {{ $v_t->team_title }}</div>
-              </div>
+              <a href="{{ url('dashboard/organization') }}/{{ $v_t->slug }}/dashboard/VS" target="_blank" class="blanklink node-name slot-active drag-impo-grab">
+                 <div class="slot-label drag-impo-grab"><span class="mr-2 d-flex badge-team-valuestream">VS <span style="font-size:22px" class="material-symbols-outlined ml-2">groups</span></span>  {{ $v_t->team_title }}</div>
+              </a>
               @foreach(DB::table('objectives')->wherenull('trash')->where('unit_id' , $v_t->id)->where('type' , 'VS')->get() as $o)
-              <div class="slot slot-inactive">
-                  <div id="connectedobjective{{ $o->id }}" class="slot-anchor-small slot-anchor-active drag-impo-grab"></div>
-                  <span class="material-symbols-outlined f-18">location_searching</span>
-                  <div class="slot-label" ><span class="label-text">{{ $o->objective_name }}</span></div>
-                  @if($o->status == 'Done')
-                  <div class="badge-done">{{round($o->obj_prog,0)}}%</div>
-                  @endif
-                  @if($o->status == 'To Do')
-                  <div class="badge-todo">{{round($o->obj_prog,0)}}%</div>
-                  @endif
-                  @if($o->status == 'In progress')
-                  <div class="badge-inprogress">{{round($o->obj_prog,0)}}%</div>
-                  @endif
-              </div>
+              <a href="{{ url('dashboard/organization') }}/{{ $v_t->slug }}/portfolio/VS?objective={{ $o->id }}" target="_blank" class="blanklink @if(DB::table('team_link_child')->where('linked_objective_id' , $o->id)->count() > 0) slot-active @else slot-inactive @endif drag-impo-grab">
+                 <div id="connectedobjective{{ $o->id }}" class="slot-anchor-small @if(DB::table('team_link_child')->where('linked_objective_id' , $o->id)->count() > 0) slot-anchor-active @else slot-anchor-inactive @endif drag-impo-grab"></div>
+                 <span class="material-symbols-outlined f-18">location_searching</span>
+                 <div class="slot-label drag-impo-grab"><span class="label-text">{{ $o->objective_name }}</span></div>
+                 @if($o->status == 'Done')
+                 <div class="badge-done">{{round($o->obj_prog,0)}}%</div>
+                 @endif
+                 @if($o->status == 'To Do')
+                 <div class="badge-todo">{{round($o->obj_prog,0)}}%</div>
+                 @endif
+                 @if($o->status == 'In progress')
+                 <div class="badge-inprogress">{{round($o->obj_prog,0)}}%</div>
+                 @endif
+              </a>
               @endforeach
             </div>
             @endforeach
@@ -159,54 +159,24 @@ $var_objective = "mapper-unit";
 </div>
 @endsection
 @section('scripts')
+<?php
+  $team_link_child_stream =   DB::table('team_link_child')->where('user_id' , Auth::id())->where('bussiness_unit_id' , $data->id)->where('from' , 'stream')->get();
+  $team_link_child_unit =   DB::table('team_link_child')->where('user_id' , Auth::id())->where('bussiness_unit_id' , $data->id)->where('from' , 'unit')->get();
+  $team_link_child_unit_or_stream = DB::table('team_link_child')->where('user_id' , Auth::id())->where('bussiness_unit_id' , $data->id)->orwhere('from' ,'stream')->where('from' , 'unit')->get();
+?>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/leader-line@1.0.5/leader-line.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/plain-draggable@2.5.12/plain-draggable.min.js"></script>
     <script type="text/javascript">
         window.addEventListener("load", function() {
   "use strict";
 
-  @php
-
-    // $team_link_child_stream =   DB::table('team_link_child')->where('user_id' , Auth::id())->where('bussiness_unit_id' , $data->id)->where('from' , 'stream')->get();
-    $team_link_child_stream = DB::table('team_link_child')
-    ->where('bussiness_unit_id' , $data->id)
-    ->where('from' , 'stream')
-    ->where(function($query) {
-        $query->where('user_id', Auth::id())
-              ->orWhere('user_id', Auth::user()->invitation_id);
-    })
-    ->get();
-    // $team_link_child_unit =   DB::table('team_link_child')->where('user_id' , Auth::id())->where('bussiness_unit_id' , $data->id)->where('from' , 'unit')->get();
-    $team_link_child_unit = DB::table('team_link_child')
-    ->where('bussiness_unit_id' , $data->id)
-    ->where('from' , 'unit')
-    ->where(function($query) {
-        $query->where('user_id', Auth::id())
-              ->orWhere('user_id', Auth::user()->invitation_id);
-    })
-    ->get();
-
-    // $team_link_child_unit_or_stream = DB::table('team_link_child')->where('user_id' , Auth::id())->where('bussiness_unit_id' , $data->id)->orwhere('from' ,'stream')->where('from' , 'unit')->get();
-    $team_link_child_unit_or_stream = DB::table('team_link_child')
-    ->where('bussiness_unit_id' , $data->id)
-    ->orwhere('from' ,'stream')
-    ->where('from' , 'unit')
-    ->where(function($query) {
-        $query->where('user_id', Auth::id())
-              ->orWhere('user_id', Auth::user()->invitation_id);
-    })
-    ->get();
-    @endphp
+  
 
   @foreach($team_link_child_stream as $t_l_c)
    var slout_out_buisness_unit_key_result_{{ $t_l_c->bussiness_key_id }} = document.getElementById("buisness_unit_key_result_{{ $t_l_c->bussiness_key_id }}");
   @endforeach
 
 
-    $team_link_child_stream =   DB::table('team_link_child')->where('user_id' , Auth::id())->where('bussiness_unit_id' , $data->id)->where('from' , 'stream')->get();
-    $team_link_child_unit =   DB::table('team_link_child')->where('user_id' , Auth::id())->where('bussiness_unit_id' , $data->id)->where('from' , 'unit')->get();
-    $team_link_child_unit_or_stream = DB::table('team_link_child')->where('user_id' , Auth::id())->where('bussiness_unit_id' , $data->id)->orwhere('from' ,'stream')->where('from' , 'unit')->get();
-  @endphp
 
   @foreach($team_link_child_stream as $t_l_c)
    var slout_out_buisness_unit_key_result_{{ $t_l_c->bussiness_key_id }} = document.getElementById("buisness_unit_key_result_{{ $t_l_c->bussiness_key_id }}");
