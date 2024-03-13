ded1d40db783d0fc8ab775a3d3213e57da3b4814
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2024-03-13 15:35:11 +0500

    fhgfgh

--- resources/views/mapper/org/buisnessunits.blade.php
+++ resources/views/mapper/org/buisnessunits.blade.php
@@ -1,10 +1,10 @@
 @foreach($business_units as $b)
 <div id="buisnessunit{{ $b->id }}" class="node buisnessunits">
-   <div class="node-name slot-active drag-impo-grab">
-      <div class="slot-label drag-impo-grab"><span style="font-size:22px" class="material-symbols-outlined">domain</span> {{ $b->business_name }}</div>
-   </div>
+   <a href="{{ url('dashboard/organization') }}/{{ $b->slug }}/dashboard/unit" target="_blank" class="blanklink node-name slot-active drag-impo-grab">
+      <div class="slot-label drag-impo-grab"><span style="font-size:22px" class="material-symbols-outlined mr-2">domain</span> {{ $b->business_name }}</div>
+   </a>
    @foreach(DB::table('objectives')->wherenull('trash')->where('unit_id' , $b->id)->where('type' , 'unit')->get() as $o)
-   <div class="@if(DB::table('team_link_child')->where('linked_objective_id' , $o->id)->count() > 0) slot-active @else slot-inactive @endif drag-impo-grab">
+   <a href="{{ url('dashboard/organization') }}/{{ $b->slug }}/portfolio/unit?objective={{ $o->id }}" target="_blank" class="blanklink @if(DB::table('team_link_child')->where('linked_objective_id' , $o->id)->count() > 0) slot-active @else slot-inactive @endif drag-impo-grab">
       <div id="connectedobjective{{ $o->id }}" class="slot-anchor-small @if(DB::table('team_link_child')->where('linked_objective_id' , $o->id)->count() > 0) slot-anchor-active @else slot-anchor-inactive @endif drag-impo-grab"></div>
       <span class="material-symbols-outlined f-18">location_searching</span>
       <div class="slot-label drag-impo-grab"><span class="label-text">{{ $o->objective_name }}</span></div>
@@ -17,22 +17,22 @@
       @if($o->status == 'In progress')
       <div class="badge-inprogress">{{round($o->obj_prog,0)}}%</div>
       @endif
-   </div>
+   </a>
    @foreach(DB::table('key_result')->wherenull('trash')->where('obj_id' , $o->id)->get() as $k)
-   <div class="slot-active">
+   <a href="{{ url('dashboard/organization') }}/{{ $b->slug }}/portfolio/unit?keyresult={{ $k->id }}" target="_blank" class="blanklink slot-active">
       <span class="material-symbols-outlined f-18 ml-2">key</span>
       <div class="slot-label"><span class="label-text">{{ $k->key_name }}</span></div>
       @if($k->key_status == 'Done')
-      <div class="badge-done mr-2"">{{round($k->key_prog,0)}}%</div>
+      <div class="badge-done mr-2">{{round($k->key_prog,0)}}%</div>
       @endif
       @if($k->key_status == 'To Do')
-      <div class="badge-todo mr-2"">{{round($k->key_prog,0)}}%</div>
+      <div class="badge-todo mr-2">{{round($k->key_prog,0)}}%</div>
       @endif
       @if($k->key_status == 'In progress')
-      <div class="badge-inprogress mr-2"">{{round($k->key_prog,0)}}%</div>
+      <div class="badge-inprogress mr-2">{{round($k->key_prog,0)}}%</div>
       @endif
       <div id="buisness_unit_key_result_{{ $k->id }}" class="slot-anchor-small @if(DB::table('team_link_child')->where('bussiness_key_id' , $k->id)->count() > 0) slot-anchor-active @else slot-anchor-inactive @endif"></div>
-   </div>
+   </a>
    @endforeach
    @endforeach
 </div>
--- resources/views/mapper/org/orgteam.blade.php
+++ resources/views/mapper/org/orgteam.blade.php
@@ -1,11 +1,11 @@
 @foreach(DB::table('org_team')->where('org_id'  , $data->id)->get() as $o_t)
 <div id="orgteam{{ $o_t->id }}" class="node orgteambox">
-   <div class="node-name slot-active drag-impo-grab">
+   <a href="{{ url('dashboard/organization') }}/{{ $o_t->slug }}/dashboard/orgT" target="_blank" class="blanklink node-name slot-active drag-impo-grab">
       <div class="slot-label drag-impo-grab"><span class="mr-2 d-flex badge-team-valuestream">ORG <span style="font-size:22px" class="material-symbols-outlined ml-2">groups</span></span>  {{ $o_t->team_title }}</div>
-   </div>
+   </a>
    @foreach(DB::table('objectives')->wherenull('trash')->where('unit_id' , $o_t->id)->where('type' , 'orgT')->get() as $o)
-   <div class="slot slot-inactive">
-      <div id="connectedobjective{{ $o->id }}" class="slot-anchor-small slot-anchor-active drag-impo-grab"></div>
+   <a href="{{ url('dashboard/organization') }}/{{ $o_t->slug }}/portfolio/orgT?objective={{ $o->id }}" target="_blank" class="blanklink @if(DB::table('team_link_child')->where('linked_objective_id' , $o->id)->count() > 0) slot-active @else slot-inactive @endif drag-impo-grab">
+      <div id="connectedobjective{{ $o->id }}" class="slot-anchor-small @if(DB::table('team_link_child')->where('linked_objective_id' , $o->id)->count() > 0) slot-anchor-active @else slot-anchor-inactive @endif drag-impo-grab"></div>
       <span class="material-symbols-outlined f-18">location_searching</span>
       <div class="slot-label" ><span class="label-text">{{ $o->objective_name }}</span></div>
       @if($o->status == 'Done')
@@ -17,7 +17,7 @@
       @if($o->status == 'In progress')
       <div class="badge-inprogress">{{round($o->obj_prog,0)}}%</div>
       @endif
-   </div>
+   </a>
    @endforeach
 </div>
 @endforeach
