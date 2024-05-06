b8292baf5a8d1ad27570b0d02227f0749078c739
basil-89 <84768962+basil-89@users.noreply.github.com>
2024-05-06 13:25:34 +0500

    Add files via upload

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -59,7 +59,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8'],
         ]);
     }
 
@@ -128,7 +128,6 @@ class RegisterController extends Controller
             'user_id' => $user->id,
             'payment_type' => 'trail',
         ]);
-
         return $user;
     }
 }
