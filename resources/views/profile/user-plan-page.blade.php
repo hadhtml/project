2f0bac8e04178d9744dc94748bafd8db57dfaad3
basil-89 <84768962+basil-89@users.noreply.github.com>
2024-05-07 19:43:28 +0500

    Add files via upload

--- resources/views/profile/user-plan-page.blade.php
+++ resources/views/profile/user-plan-page.blade.php
@@ -15,21 +15,21 @@ $organization  = DB::table('organization')->where('user_id',Auth::id())->orWhere
             </div>
             <div class="row g-10">
                 @php
-                $plan = DB::table('plan')->where('status','Active')->where('base_price_status', '!=','free')->orderby('id','DESC')->limit(3)->get();
+                $plan = DB::table('plan')->where('status','Active')->orderby('id','DESC')->get();
                 @endphp
-                @foreach($plan as $key => $p)
+                @foreach($plan as $p)
                 @php
                 $dataArray = explode(',', $p->module);
                 @endphp
                 <div class="col-xl-4">
-                    <div class="d-flex h-100 align-items-center" >
-                        <div class="w-100 d-flex flex-column flex-center rounded-3 @if($key == 1) bg-primary text-white @else bg-light @endif bg-opacity-75 py-15 px-10" >
-                            <div class="mb-7 text-center" >
-                                <h1 class=" mb-5 fw-bolder @if($key == 1) text-white @else text-gray-900 @endif" >{{$p->plan_title}}</h1>
+                    <div class="d-flex h-100 align-items-center">
+                        <div class="w-100 d-flex flex-column flex-center rounded-3 bg-light bg-opacity-75 py-15 px-10">
+                            <div class="mb-7 text-center">
+                                <h1 class="text-gray-900 mb-5 fw-bolder">{{$p->plan_title}}</h1>
                                 <div class="text-center">
                                     @if($p->base_price_status ==  'price')
                                     <span class="mb-2 text-primary">£</span>
-                                    <span class="fs-3x fw-bold  @if($key == 1) text-white @else text-primary @endif">{{$p->base_price}}</span>
+                                    <span class="fs-3x fw-bold text-primary">{{$p->base_price}}</span>
                                     <span class="fs-7 fw-semibold opacity-50">/ 
                                     <span data-kt-element="period">{{$p->billing_method}}</span></span>
                                     @else
@@ -40,12 +40,12 @@ $organization  = DB::table('organization')->where('user_id',Auth::id())->orWhere
                             <div class="w-100 mb-10">
                                 @foreach($dataArray as $arr)
                                 <div class="d-flex align-items-center mb-5">
-                                    <span class="fw-semibold fs-6  flex-grow-1 pe-3 @if($key == 1) text-white @else text-gray-800 @endif">{{$arr}}</span>
+                                    <span class="fw-semibold fs-6 text-gray-800 flex-grow-1 pe-3">{{$arr}}</span>
                                     <i class="ki-outline ki-check-circle fs-1 text-success"></i>
                                 </div>
                                 @endforeach
                             </div>
-                            <a href="{{url('boost-payment/'.$p->plan_id)}}" class="btn btn-sm btn-primary  @if($key == 1) bg-white text-black @else btn-primary @endif">Select</a>
+                            <a href="{{url('boost-payment/'.$p->plan_id)}}" class="btn btn-sm btn-primary">Select</a>
                         </div>
                     </div>
                 </div>
