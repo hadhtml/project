5b4bbf79d2bb8fa50dd495f117f1cb87c0a81ee5
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2024-05-09 14:00:38 +0500

    asdsad

--- resources/views/components/main-layout.blade.php
+++ resources/views/components/main-layout.blade.php
@@ -26,6 +26,7 @@
         <script src="{{url('public/assets/Random-Pixel/dist/gixi-min.js')}}"></script>
         <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/style.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/styletwo.css') }}">
+        <script src="//code.tidio.co/iuyaftwehxdry2ttyq7ozimm6gykitnw.js" async></script>
     </head>
     <body id="kt_app_body" data-kt-app-header-fixed="true" data-kt-app-header-fixed-mobile="true" data-kt-app-sidebar-enabled="true" data-kt-app-sidebar-fixed="true" data-kt-app-sidebar-hoverable="true" data-kt-app-sidebar-push-toolbar="true" data-kt-app-sidebar-push-footer="true" data-kt-app-aside-enabled="true" data-kt-app-aside-fixed="true" data-kt-app-aside-push-toolbar="true" data-kt-app-aside-push-footer="true" class="app-default">
         <script>var defaultThemeMode = "light"; var themeMode; if ( document.documentElement ) { if ( document.documentElement.hasAttribute("data-bs-theme-mode")) { themeMode = document.documentElement.getAttribute("data-bs-theme-mode"); } else { if ( localStorage.getItem("data-bs-theme") !== null ) { themeMode = localStorage.getItem("data-bs-theme"); } else { themeMode = defaultThemeMode; } } if (themeMode === "system") { themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"; } document.documentElement.setAttribute("data-bs-theme", themeMode); }</script>
